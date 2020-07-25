e091e9e9cb3567a8f3346fde15212397dd45ca90
Farad2020 <fnirc@mail.ru>
2020-07-25 07:38:03 +0600

    Responsiveness and color themes added

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,17 @@
+document.getElementById('change-theme-btn').addEventListener('click', function(){
+    if(document.documentElement.getAttribute('data-theme') == 'light' ){
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+    }else{
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+
+    }
+});
+
+let trans = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
+}
